c404cc771d0ac002be1c6cb09a89ba49782ea223
darwincrack <erudito_good@hotmail.com>
2024-07-23 16:32:28 -0400

    se amplia en contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
 
-  <div className="container my-5">
+  <div className="container-fluid my-5">
     <div className="p-5  bg-body-tertiary rounded-3">
         <h1 className="text-center">Feedback form</h1>
         <Form />

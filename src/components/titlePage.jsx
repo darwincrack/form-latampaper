1c35de4601ed1d10376eb08bffac06bbbdb0e71c
darwincrack <erudito_good@hotmail.com>
2024-07-24 16:36:19 -0400

    se quito nav de idiomas

--- src/components/titlePage.jsx
+++ src/components/titlePage.jsx
@@ -6,15 +6,6 @@ function titlePage(props) {
   return (
     <>
       <h1 className="text-center">{props.titulo}</h1>
-        <div className="text-center">
-        Language:
-          <Button href="/" className="m-2"  size="sm">English</Button>
-          <Button href="/es" className="m-2"  size="sm"> Spanish</Button>
-          <Button href="/port" className="m-2"  size="sm">Port</Button>
-        </div>
-
-
-
     </>
   )
 }

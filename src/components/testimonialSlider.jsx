d725834c715878658b2820eac189f31444de181f
darwincrack <erudito_good@hotmail.com>
2024-10-25 16:56:14 -0400

    update fix

--- src/components/testimonialSlider.jsx
+++ src/components/testimonialSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,6 +6,16 @@ import '../styles.css';
 import parse from 'html-react-parser';
 
 const TestimonialSlider = (props) => {
+
+  
+  useEffect(function () {
+    document.body.style.backgroundColor = '#2c299f';
+
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  })
+
 
   const testimonios = props.testimonios;
   const settings = {
